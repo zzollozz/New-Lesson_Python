fe2f9e91e542fa1ca3cf4977eb7c9c9cdbd73b54
zzollozz <81852084+zzollozz@users.noreply.github.com>
2023-03-16 20:18:41 +0300

    Выполнил ДЗ_8

--- hw_8/lesson_8.py
+++ hw_8/lesson_8.py
@@ -6,12 +6,17 @@
 # 6. Изменить контакт
 # 7. Удалить контакт
 # 8. Выход
+
 import json
 import os
+import re
 from pprint import pprint
 
 
 class PhoneBook:
+    """
+    Телефонная книга
+    """
     path = os.getcwd()
 
     def __init__(self, name_file):
@@ -30,6 +35,22 @@ class PhoneBook:
             json.dump(data_for_save, file, indent=4)
             return f'файл {name_file} обновлен'
 
+    def find_contact(self, firstName=None, surname=None, phone_number=None):
+        id_list = []
+        for id_key, cont in self.phonebook.items():
+            if firstName != None:
+                if cont.get('firstName') == firstName:
+                    id_list.append(id_key)
+            elif surname != None:
+                if cont.get('surname') == surname:
+                    id_list.append(id_key)
+            elif phone_number != None:
+                if cont.get('phone_number') == phone_number:
+                    id_list.append(id_key)
+        print()
+        id_list = set(id_list)
+        return id_list
+
     def create_contact(self, firstName, surname, phone_number):
         """
         Создание Контакта
@@ -38,32 +59,66 @@ class PhoneBook:
         :param phone_number:
         :return:
         """
-        self.phonebook[str(len(self.phonebook.keys()))] = {'firstName': firstName,
-                                                           'surname': surname,
-                                                           'phone_number': [phone_number]
-                                                           }
-        print(f'контакт id {len(self.phonebook.keys())}: {firstName}  добавлен')
+        id_key = len(self.phonebook.keys())
+        if self.phonebook.get(str(id_key)):
+            while self.phonebook.get(str(id_key)):
+                id_key += 1
+        if ' ' in re.findall(r'[,;:+ -]', phone_number):
+            phone_number = list(map(lambda x: int(x.strip()), phone_number.split(re.findall(r'[,;:+ -]', phone_number)[0])))
+
+        contact = self.read_contacts(firstName, surname, phone_number)
+
+        if contact != None:
+            if contact.get('firstName') == firstName and contact.get('surname') == surname and contact.get('phone_number') == phone_number:
+                 return f"контакт с таким именем {firstName} и фамилией {surname} и номером телефона есть"
+
+        else:
+            self.phonebook[id_key] = {'firstName': firstName,
+                                      'surname': surname,
+                                      'phone_number': phone_number
+                                      }
+            print(f'контакт id {id_key}: {firstName}  добавлен')
+
+            return self.save_file(self.name_file, self.phonebook)
+
+    def update_contact(self, id_contact, new_firstName=None, new_surname=None, new_phone_number=None):
+        """
+        Обновление контакта
+        :param id_contact:
+        :param new_firstName:
+        :param new_surname:
+        :param new_phone_number:
+        :return:
+        """
+        contact = self.phonebook.get(id_contact)
+
+        firstName = new_firstName if new_firstName != None else contact.get('firstName')
+        surname = new_surname if new_surname != None else contact.get('surname')
+        phone_number = new_phone_number if new_phone_number != None else contact.get('phone_number')
+
+        update_contact = {'firstName': firstName,
+                          'surname': surname,
+                          'phone_number': phone_number
+                          }
+        self.phonebook[id_contact] = update_contact
+        print(f'Контакт id: {id_contact} {firstName} {surname} обновлен ')
         return self.save_file(self.name_file, self.phonebook)
 
-    def update_contact(self, id_contact, firstName=None, surname=None, phone_number=None):
-
-        name_file = "sdsd"
-        update_contact = {'firstName': firstName,
-                    'surname': surname,
-                    'phone_number': [phone_number]
-                    }
-        self.phonebook[id_contact] = update_contact
-        return self.save_file(self, name_file, self.phonebook)
-
-    def read_contacts(self, get_id=None):
+    def read_contacts(self, new_firstName=None, new_surname=None, new_phone_number=None):
         """
         Вывод всех контактов Телефоннй Книги
         или
         :param get_id: -> поиск по ID контакта
         :return:
         """
-        if get_id:
-            return self.phonebook.get(get_id)
+        if new_firstName or new_surname or new_phone_number:
+            id_list_cont = self.find_contact(new_firstName, new_surname, new_phone_number)
+            if id_list_cont:
+                for cont in id_list_cont:
+                    print(f'Имя: {self.phonebook.get(str(cont)).get("firstName")}, Фамилия: {self.phonebook.get(str(cont)).get("surname")}, Телефон: {self.phonebook.get(str(cont)).get("phone_number")}')
+                    return self.phonebook.get(str(cont))
+            else:
+                return None
         else:
             return self.phonebook
 
@@ -80,18 +135,17 @@ class PhoneBook:
             del self.phonebook[id_contact]
             return self.save_file(self.name_file, self.phonebook)
 
-    def find_contact(self, id_contact):
-        id_contact = id_contact
-        contact = self.phonebook.get(id_contact)
-        return
+
 
     #   Результат:
 
 
-phonebook = PhoneBook('phonebook.json')
+phonebook = PhoneBook('phonebook.json')  # объект класса с аргументом названия файла тел. книги
 
-# pprint(phonebook.read_contacts('1')) # Вывод Нучного контакта
-# pprint(phonebook.create_contact('Smit', 'Shhhhh', '77772341178')) # Создание Нового контакта
-# pprint(phonebook.read_contacts()) # Вывод всех контактов книги
-pprint(phonebook.delete_contact('5')) # Удаление
-pprint(phonebook.update_contact('6', '70009991188')) # Удаление
+# pprint(phonebook.read_contacts('Smit')) # Вывод Нужного контакта
+# pprint(phonebook.create_contact('Smit', 'Shhhhh', "79991112233, 453453453453"))  # Создание Нового контакта в телефонной книге
+# pprint(phonebook.create_contact('Bob', 'Boby', "79991112233"))  # Создание Нового контакта в телефонной книге
+# pprint(phonebook.create_contact('Tom', 'Tomy', "74546456456747"))  # Создание Нового контакта в телефонной книге
+# pprint(phonebook.read_contacts())  # Вывод всех контактов книги
+# pprint(phonebook.delete_contact('2'))  # Удаление контакта в телефонной книге
+pprint(phonebook.update_contact('3', None, 'UPDATE', '70000000000'))  # Обновление данных контакта в телефонной книге
